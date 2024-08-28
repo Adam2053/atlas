8083529a8d23a1e8fe740e5dc1ca1d7a0d122e61
Arpit Marathe <arpitmarathe2002@gmail.com>
2024-08-29 02:39:54 +0530

    changed the base url

--- client/src/components/Country.jsx
+++ client/src/components/Country.jsx
@@ -23,7 +23,7 @@ const Country = () => {
   const fetchFlag = async () => {
     try {
       const response = await axios
-        .get("http://localhost:3000/random")
+        .get("https://atlas-backend-omega.vercel.app/random")
         .then((res) => res.data.data);
 
       setCountryData(response);
