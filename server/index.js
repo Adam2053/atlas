9752ef6ebe9a64a489e005ad749f4d0880166360
Arpit Marathe <arpitmarathe2002@gmail.com>
2024-08-29 02:49:01 +0530

    added cors policy v2

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://atlas-game-beta.vercel.app/'
+  origin: 'https://atlas-game-beta.vercel.app'
 }));
 app.use(express.json());
 
